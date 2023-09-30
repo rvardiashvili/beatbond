d159e550b4402c052907c7c11af5e73b7bcb463f
Rati Vardiashvili <vardiashvilirati33@gmail.com>
2023-09-30 19:24:27 +0200

    Schema

--- Schema.sql
+++ Schema.sql
@@ -1,120 +1,132 @@
-    CREATE TABLE Users(
-        uid INT NOT NULL UNIQUE,
-        name CHAR(255) NOT NULL,
-        lastname CHAR(255) NOT NULL,
-        gender CHAR(255) NOT NULL,
-        dob DATE NOT NULL,
-        profile_picture LONGBLOB NOT NULL,
-        email CHAR(255) NOT NULL UNIQUE,
-        password CHAR(255),
-        city CHAR(255) NOT NULL, 
-        PRIMARY KEY(uid)
-    );
+CREATE TABLE Users(
+    uid INT NOT NULL UNIQUE,
+    name CHAR(255) NOT NULL,
+    lastname CHAR(255) NOT NULL,
+    gender CHAR(255) NOT NULL,
+    dob DATE NOT NULL,
+    profile_picture LONGBLOB NOT NULL,
+    email CHAR(255) NOT NULL UNIQUE,
+    password CHAR(255),
+    city CHAR(255) NOT NULL, 
+    PRIMARY KEY(uid)
+);
 
-    CREATE TABLE Preferences(
-        uid INT NOT NULL,
-        relationshipType CHAR(255) NOT NULL,
-        gender CHAR(1),
-        minAge INT NOT NULL,
-        maxAge INT NOT NULL,
-        muscial_genre CHAR(255) NOT NULL,
-        PRIMARY KEY(uid),
-        FOREIGN KEY(uid)
-        REFERENCES Users(uid)
-    );
+CREATE TABLE PossibleMatches(
+    mid INT NOT NULL UNIQUE,
+    uid INT NOT NULL,
+    PRIMARY KEY (mid),
+    Foreign Key (uid) REFERENCES Users(uid)
+);
 
-    CREATE TABLE MusicalProfile(
-        mpid INT NOT NULL UNIQUE,
-        genre CHAR(255) NOT NULL,
-        artist CHAR(255) NOT NULL,
-        abloum CHAR(255) NOT NULL,
-        song CHAR(255) NOT NULL,
-        vibe CHAR(255) NOT NULL,
-        PRIMARY KEY(mpid)
-    );
+CREATE TABLE Preferences(
+    uid INT NOT NULL,
+    relationshipType CHAR(255) NOT NULL,
+    gender CHAR(1),
+    minAge INT NOT NULL,
+    maxAge INT NOT NULL,
+    muscial_genre CHAR(255) NOT NULL,
+    PRIMARY KEY(uid),
+    FOREIGN KEY(uid)
+    REFERENCES Users(uid)
+);
 
-    CREATE TABLE Swipes(
-        SwipeId INT NOT NULL UNIQUE,
-        uid INT NOT NULL,
-        liked BOOL,
-        PRIMARY KEY(SwipeId),
-        FOREIGN KEY(uid)
-        REFERENCES Users(uid)
-    );
+CREATE TABLE MusicalProfile(
+    mpid INT NOT NULL UNIQUE,
+    genre CHAR(255) NOT NULL,
+    artist CHAR(255) NOT NULL,
+    abloum CHAR(255) NOT NULL,
+    song CHAR(255) NOT NULL,
+    vibe CHAR(255) NOT NULL,
+    PRIMARY KEY(mpid)
+);
 
-    CREATE TABLE Messeges(
-        chatId INT NOT NULL,
-        message TEXT NOT NULL,
-        date DATE NOT NULL,
-        PRIMARY KEY(ChatId)
-    );
+CREATE TABLE Swipes(
+    SwipeId INT NOT NULL UNIQUE,
+    uid INT NOT NULL,
+    liked BOOL,
+    PRIMARY KEY(SwipeId),
+    FOREIGN KEY(uid)
+    REFERENCES Users(uid)
+);
 
-    CREATE TABLE Matches(
-        uid INT NOT NULL,
-        matchId INT NOT NULL UNIQUE,
-        PRIMARY KEY(matchId),
-        Foreign Key (uid)
-        REFERENCES Users(uid)
-    );
+CREATE TABLE Messeges(
+    chatId INT NOT NULL,
+    message TEXT NOT NULL,
+    date DATE NOT NULL,
+    PRIMARY KEY(ChatId)
+);
 
-    CREATE TABLE BlockedUsers(
-        uid INT NOT NULL,
-        blockId INT NOT NULL UNIQUE,
-        PRIMARY KEY(blockId),
-        FOREIGN KEY(uid)
-        REFERENCES Users(uid)
-    );
+CREATE TABLE Matches(
+    uid INT NOT NULL,
+    matchId INT NOT NULL UNIQUE,
+    PRIMARY KEY(matchId),
+    Foreign Key (uid)
+    REFERENCES Users(uid)
+);
 
-    CREATE TABLE Pictures(
-        uid INT NOT NULL,
-        pictureId INT NOT NULL UNIQUE,
-        picture LONGBLOB NOT NULL,
-        PRIMARY KEY(pictureId),
-        Foreign Key (uid)
-        REFERENCES Users(uid)
-    );
+CREATE TABLE BlockedUsers(
+    uid INT NOT NULL,
+    blockId INT NOT NULL UNIQUE,
+    PRIMARY KEY(blockId),
+    FOREIGN KEY(uid)
+    REFERENCES Users(uid)
+);
 
-    CREATE TABLE Massaged_to(
-        senderUid INT NOT NULL,
-        recieverUid INT NOT NULL,
-        chatId INT NOT NULL,
-        PRIMARY KEY(senderUid, recieverUid, chatId),
-        Foreign Key (senderUid) REFERENCES Users(uid),
-        Foreign Key (recieverUid) REFERENCES Users(uid),
-        Foreign Key (chatId) REFERENCES Messeges(chatId)
-    );
+CREATE TABLE Pictures(
+    uid INT NOT NULL,
+    pictureId INT NOT NULL UNIQUE,
+    picture LONGBLOB NOT NULL,
+    PRIMARY KEY(pictureId),
+    Foreign Key (uid)
+    REFERENCES Users(uid)
+);
 
-    CREATE TABLE Blocked_Users (
-        uid INT NOT NULL,
-        blockId INT NOT NULL,
-        PRIMARY KEY(uid, blockID),
-        FOREIGN KEY (Uid) REFERENCES Users (uid),
-        Foreign Key (blockId) REFERENCES BlockedUsers(blockId)
-    );
+CREATE TABLE Massaged_to(
+    senderUid INT NOT NULL,
+    recieverUid INT NOT NULL,
+    chatId INT NOT NULL,
+    PRIMARY KEY(senderUid, recieverUid, chatId),
+    Foreign Key (senderUid) REFERENCES Users(uid),
+    Foreign Key (recieverUid) REFERENCES Users(uid),
+    Foreign Key (chatId) REFERENCES Messeges(chatId)
+);
 
-    CREATE TABLE Matched_Users_With (
-        uid INT NOT NULL,
-        matchId INT,
-        PRIMARY KEY(uid, matchId),
-        FOREIGN KEY (uid) REFERENCES Users (uid),
-        FOREIGN KEY (matchId) REFERENCES Matches(matchId)
-    );
+CREATE TABLE Blocked_Users (
+    uid INT NOT NULL,
+    blockId INT NOT NULL,
+    PRIMARY KEY(uid, blockID),
+    FOREIGN KEY (Uid) REFERENCES Users (uid),
+    Foreign Key (blockId) REFERENCES BlockedUsers(blockId)
+);
 
-    CREATE TABLE Swiped_On (
-        swiperUid INT NOT NULL,
-        swipeId INT NOT NULL,
-        PRIMARY KEY (SwiperUid, SwipeId),
-        FOREIGN KEY (SwiperUid) REFERENCES Users (uid),
-        FOREIGN KEY (SwipeId) REFERENCES Swipes (SwipeId)
-    );
+CREATE TABLE Matched_Users_With (
+    uid INT NOT NULL,
+    matchId INT,
+    PRIMARY KEY(uid, matchId),
+    FOREIGN KEY (uid) REFERENCES Users (uid),
+    FOREIGN KEY (matchId) REFERENCES Matches(matchId)
+);
 
+CREATE TABLE Swiped_On (
+    swiperUid INT NOT NULL,
+    swipeId INT NOT NULL,
+    PRIMARY KEY (SwiperUid, SwipeId),
+    FOREIGN KEY (SwiperUid) REFERENCES Users (uid),
+    FOREIGN KEY (SwipeId) REFERENCES Swipes (SwipeId)
+);
 
-    CREATE TABLE Likes_Music (
-        uid INT NOT NULL,
-        mpid INT NOT NULL,
-        PRIMARY KEY (uid, mpid),
-        FOREIGN KEY (uid) REFERENCES Users (uid),
-        FOREIGN KEY (mpid) REFERENCES MusicalProfile (mpid)
-    );
+CREATE TABLE Likes_Music (
+    uid INT NOT NULL,
+    mpid INT NOT NULL,
+    PRIMARY KEY (uid, mpid),
+    FOREIGN KEY (uid) REFERENCES Users (uid),
+    FOREIGN KEY (mpid) REFERENCES MusicalProfile (mpid)
+);
 
-
+CREATE TABLE has_possible_matches(
+    uid INT NOT NULL,
+    mid INT NOT NULL,
+    PRIMARY KEY(uid, mid),
+    Foreign Key (uid) REFERENCES Users(uid),
+    Foreign Key (mid) REFERENCES PossibleMatches(mid)
+);
